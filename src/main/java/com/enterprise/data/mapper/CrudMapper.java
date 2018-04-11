fdc797b49fe40dfe2d13c8fa0ef531e7722824c2
Tommy <ggffwo@126.com>
2018-04-11 13:33:13 +0800

    提交：添加自动生成的接口方法

--- src/main/java/com/enterprise/data/mapper/CrudMapper.java
+++ src/main/java/com/enterprise/data/mapper/CrudMapper.java
@@ -10,6 +10,31 @@ import java.util.List;
  */
 public interface CrudMapper<T, PK extends Serializable> {
 
+
+    /**
+     * 插入一个新的对象
+     *
+     * @param record 对应的记录
+     * @return 返回影响的行数，成功返回 1
+     */
+    int insert(T record);
+
+    /**
+     * 根据条件插入一个新的对象
+     *
+     * @param record 对应的记录
+     * @return 返回影响的行数，成功返回 1
+     */
+    int insertSelective(T record);
+
+    /**
+     * 批量插入一组新的对象
+     *
+     * @param list 对应的记录列表
+     * @return 返回影响的行数，成功返回 1
+     */
+    int batchInsert(List<T> list);
+
     /**
      * 根据id删除对象
      *
@@ -19,12 +44,20 @@ public interface CrudMapper<T, PK extends Serializable> {
     int deleteById(PK id);
 
     /**
-     * 插入一个新的对象
+     * 根据id更新对象
      *
-     * @param record 对应的记录
-     * @return 返回影响的行数，成功返回 1
+     * @param record
+     * @return 返回影响的行数，失败返回0
      */
-    int insert(T record);
+    int updateById(T record);
+
+    /**
+     * 根据id更新非空字段
+     *
+     * @param record
+     * @return 返回影响的行数，失败返回0
+     */
+    int updateBySelective(T record);
 
     /**
      * 根据id查询对象
@@ -42,10 +75,11 @@ public interface CrudMapper<T, PK extends Serializable> {
     List<T> list();
 
     /**
-     * 根据id更新对象
+     * 根据对象条件查询，取出所有记录
      *
-     * @param record
-     * @return 返回影响的行数，失败返回0
+     * @param record 对象条件
+     * @return 返回列表
      */
-    int updateById(T record);
+    List<T> listByEntityWhere(T record);
+
 }
